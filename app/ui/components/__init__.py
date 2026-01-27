18ff7ecc0be04561d0347a5e8c41b77ecde677eb
nhieelam <lam.nhiee11@gmail.com>
2026-01-27 13:42:01 +0700

    package for components

---
+++ app/ui/components/__init__.py
@@ -0,0 +1,10 @@
+from .sidebar import render_sidebar
+from .chat_display import render_chat_header, render_chat_messages
+from .chat_input import render_chat_input
+
+__all__ = [
+    "render_sidebar",
+    "render_chat_header",
+    "render_chat_messages",
+    "render_chat_input",
+]
