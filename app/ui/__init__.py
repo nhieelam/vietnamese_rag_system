d9bfd7af236977ad01504a512884d783457b4aec
nhieelam <lam.nhiee11@gmail.com>
2026-01-27 13:43:41 +0700

    package for component and css

---
+++ app/ui/__init__.py
@@ -0,0 +1,16 @@
+from .styles import apply_custom_styles
+from .components import (
+    render_sidebar,
+    render_chat_header,
+    render_chat_messages,
+    render_chat_input,
+)
+
+__all__ = [
+    "apply_custom_styles",
+    "render_sidebar",
+    "render_chat_header",
+    "render_chat_messages",
+    "render_chat_input",
+
+]
