6ae405be039b50d257e2152911dff1fced82cd4c
camdao <luongcamdao@gmail.com>
2026-01-28 18:41:46 +0700

    refactor: remove unused chat routes and services from API

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -2,11 +2,8 @@
 
 from .session_manager import initialize_session_state
 from .file_loader import extract_text_from_file
-from .generate_answer_service import generate_answer, GenerateAnswerService
 
 __all__ = [
     "initialize_session_state",
     "extract_text_from_file",
-    "generate_answer",
-    "GenerateAnswerService"
 ]
