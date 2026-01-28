ccd4d4b86d5dd9e268eccd1e4e568058f359f93c
nhieelam <lam.nhiee11@gmail.com>
2026-01-28 13:58:54 +0700

    turn text into chunks, turn chunks into vector

---
+++ app/services/ingest_service.py
@@ -0,0 +1,38 @@
+import os
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_core.documents import Document
+from app.services.ocr_service import extract_text_from_image
+from app.config.app_config import AppConfig
+
+DB_DIRECTORY = AppConfig.DB_DIRECTORY
+
+def ingest_file(raw_text):
+    
+    if not raw_text.strip():
+        print("No text found !")
+        return
+
+    print("Text extracted. Splitting into chunks...")
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        AppConfig.CHUNK_SIZE,
+        AppConfig.CHUNK_OVERLAP
+    )
+    docs = text_splitter.create_documents([raw_text])
+
+    print("Saving to ChromaDB...")
+    embedding_model = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
+    )
+    
+    # We use 'add_documents' to append to the existing DB, not overwrite it
+    vector_db = Chroma(
+        persist_directory=DB_DIRECTORY, 
+        embedding_function=embedding_model
+    )
+    vector_db.add_documents(docs)
+    
+    print(f"Success! Added {len(docs)} chunks to the database.")
+
