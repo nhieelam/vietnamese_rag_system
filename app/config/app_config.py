e4d3d4763d74657eff89a43244feb4d174c2d4bf
nhieelam <lam.nhiee11@gmail.com>
2026-01-28 16:37:44 +0700

    up

--- app/config/app_config.py
+++ app/config/app_config.py
@@ -1,6 +1,5 @@
+import os
 import streamlit as st
-
-
 
 class AppConfig:
 
@@ -15,6 +14,9 @@ class AppConfig:
     MIN_MAX_TOKENS = 100
     MAX_MAX_TOKENS = 2000
     MAX_TOKENS_STEP = 100
+
+    CHUNK_SIZE = 500
+    CHUNK_OVERLAP = 50
     
     CHAT_MESSAGE_MAX_WIDTH = "70%"
     USER_MESSAGE_BG_COLOR = "#007bff"
@@ -22,3 +24,11 @@ class AppConfig:
     
     TIMESTAMP_FORMAT = "%H:%M"
     UPLOAD_TIMESTAMP_FORMAT = "%Y-%m-%d %H:%M"
+
+    DB_DIRECTORY = "./chroma_db"
+    
+    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
+    GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY", "")
+    
+    LLM_MODEL = "gpt-3.5-turbo"  
+    LLM_TEMPERATURE = 0.3
