343b3641b84ffacae18b5f76a6cb8be3e310c777
camdao <luongcamdao@gmail.com>
2026-02-04 20:05:34 +0700

    refactor:  remove parameter

--- app/orc/paddle_wrapper.py
+++ app/orc/paddle_wrapper.py
@@ -1,15 +1,11 @@
-from typing import Union, List, Tuple, Optional
-from paddleocr import PaddleOCR, draw_ocr
-from PIL import Image
-import numpy as np
-import io
+from paddleocr import PaddleOCR
 
 class PaddleOCRWrapper:
-    def __init__(self, lang='vi', use_angle_cls=True, use_gpu=False, **kwargs):
+    def __init__(self, lang='vi', use_angle_cls=True, **kwargs):
         self.lang = lang
         self.use_angle_cls = use_angle_cls
-        self.use_gpu = use_gpu
-        self._ocr = PaddleOCR(use_angle_cls=use_angle_cls, lang=lang, use_gpu=use_gpu, **kwargs)
-
-    def read_image(self, image, cls=True):
-        return self._ocr.predict(image, cls=cls)
+        self._ocr = PaddleOCR(use_angle_cls=use_angle_cls, lang=lang,  **kwargs)
+    
+    def read_image(self, image):
+        return self._ocr.predict(image)
+    
